57b5568693e83a280e0f0917a83cdb6e8238a484
Yuri <admin@clipics.com>
2026-01-03 12:28:05 +0100

    final commit

--- posts/views.py
+++ posts/views.py
@@ -1,32 +1,48 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 from .models import Post
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
-class PostList(ListView):
+class PostList(LoginRequiredMixin, ListView):
     model = Post
     template_name = "post_list.html"
 
 
-class PostDetail(DetailView):
+class PostDetail(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "posts/post_detail.html"
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     template_name = "posts/post_new.html"
-    fields = ["title", "author", "body"]
+    fields = ["title", "body"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
-class PostUpdate(UpdateView):
+class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = "posts/post_edit.html"
     fields = ["title", "body"]
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class PostDelete(DeleteView):
+
+class PostDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = "posts/post_delete.html"
     success_url = reverse_lazy("posts_home")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
